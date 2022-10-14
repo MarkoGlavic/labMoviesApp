4fd09a86eebe58fb22160b4d739b8f9e22686844
Marko Glavic <glavim@gmail.com>
2022-10-14 14:31:00 +0100

    Add to favourites logic completed; Dummy favourite page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -22,6 +22,13 @@ const MovieListPage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
     useEffect(() => {
       fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -50,7 +57,7 @@ const MovieListPage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
       </Grid>
     </Grid>
   );
