8f73e82ad011e0bd09d2a441581ac69eaa088bcc
Marko Glavic <glavim@gmail.com>
2022-10-28 15:49:34 +0100

    doesnt work

--- src/index.js
+++ src/index.js
@@ -8,10 +8,23 @@ import {Link} from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import UpcomingPage from "./pages/upcomingPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 
     const App = () => {
       return (
+        <QueryClientProvider client={queryClient}>
           <BrowserRouter>
               <SiteHeader />     
               <Routes>
@@ -23,6 +36,8 @@ import UpcomingPage from "./pages/upcomingPage";
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
